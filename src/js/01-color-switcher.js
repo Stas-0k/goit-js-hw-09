975274a821be2915a7b81bd23c0d084d02a6aa98
Stas-0k <brickbybrick15@gmail.com>
2022-11-10 00:02:07 +0200

    commit 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBttn = document.querySelector('[data-start]')
+const stopBttn = document.querySelector('[data-stop]')
+const bodyEl = document.querySelector('body')
+let idInt = 0; 
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackground() { 
+    idInt = setInterval(() => { bodyEl.style.backgroundColor = getRandomHexColor() }, 1000);
+    startBttn.disabled = 'true'
+}
+
+startBttn.addEventListener('click', changeBackground)
+stopBttn.addEventListener('click', stopChange)
+
+function stopChange() { 
+    clearInterval(idInt)
+    startBttn.removeAttribute('disabled')
+}
+
 
